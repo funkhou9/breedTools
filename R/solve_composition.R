a4c2b8a170db68ee2daeef98cd2cc87110d92495
Scott Funkhouser <sfunkhouser@gmail.com>
2015-07-01 11:05:42 -0400

    Better prepping of input for QPsolve (dosage is halved) and QPsolve_par (doseage is not halved)

--- R/solve_composition.R
+++ R/solve_composition.R
@@ -4,7 +4,9 @@
 #'  batch of animals.
 #' 
 #' @param Y numeric matrix of genotypes (rows) from all animals (columns) in population
-#' @param X numeric matrix of allele frequencies from reference animals
+#'  coded as dosage of allele B {0, 1, 2}
+#' @param X numeric matrix of allele frequencies from reference animals. Frequencies are
+#'  relative to allele B.
 #' @param names character vector of names for each breed/ancestral population in the order
 #'  that they appear in X.
 #' @param ped data.frame giving pedigree information. Must be formatted "ID", "Sire", "Dam"
@@ -19,22 +21,36 @@ solve_composition <- function(Y, X, names, ped = NULL, groups = NULL) {
   # At the moment can only have ped OR groups. They are not currently compatable
   #   although they should be
   
-  # If ped is not null, use QPsolve_par to compute genomic composition using
-  #   only animals who have genotyped parents (using the parental information).
+  # If ped is supplied, use QPsolve_par to compute genomic composition using
+  #   only animals who have genotyped parents (by incorporating Sire genotype).
   if (!is.null(ped)) {
-    mat_results <- sapply(colnames(Y), QPsolve_par, Y, X, ped)
+    mat_results <- lapply(colnames(Y), QPsolve_par, Y, X, ped)
     mat_results_tab <- do.call(rbind, mat_results)
+    
     return (mat_results_tab)
     
-  # Else if groups is not null - perform separated computation by listings in groups 
+  # Else if groups supplied - perform regular genomic computation
+  #   and list results by groups
   } else if (!is.null(groups)) {
-    grouped_results <- lapply(groups, QPseparate, Y8K, X8K)
+    
+    # When using regular genomic computation - dosage must first be halved
+    Y <- Y/2
+    
+    grouped_results <- lapply(groups, QPseparate, Y, X)
     return (grouped_results)
   
   # If neither using the ped or grouping option - just perform normal, unsegregated
   #   calculation
   } else {
+    
+    # When using regular genomic computation - dosage must first be halved
+    Y <- Y/2
+    
     results <- t(apply(Y, 2, QPsolve, X))
     return (results)
   }
 }
+
+
+
+
