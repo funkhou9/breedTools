763894be158f81e16f254a89c88c0b954df46306
Scott Funkhouser <sfunkhouser@gmail.com>
2015-06-30 23:54:03 -0400

    In working order and retains animal IDs

--- R/QPsolve_par.R
+++ R/QPsolve_par.R
@@ -11,6 +11,7 @@
 # @param names character names of breeds
 # @return data.frame of breed composition estimates
 # @import quadprog
+# @export
 QPsolve_par <- function(id, Y, X, ped,
                         p = 4,
                         names = c("Duroc", "Hampshire", "Landrace", "Yorkshire")) {
@@ -26,8 +27,9 @@ QPsolve_par <- function(id, Y, X, ped,
     sire_id <- ped[ped[, 1] == id, "Sire"]
     
     if (sire_id %in% colnames(Y)) {
+    
       # Get genotype of target animal and convert missing genotypes
-      #	to the appropriate missing character
+      #	to the appropriate missing character.
       geno <- Y[, colnames(Y) == id]
       geno[is.na(geno)] <- '-'
       
@@ -40,11 +42,21 @@ QPsolve_par <- function(id, Y, X, ped,
       mat_hap <- mapply(mat_allele, geno, sire_geno)
       
       # Remove non-numerics from maternal haplotype
-      # (NA or "?"), remove those SNPs from X and convert Y to numeric
+      # ("?") and convert to numeric. 
       Ymod <- mat_hap[mat_hap != '?']
+      Ymod_num <- as.numeric(Ymod)
+      
+      # Ensure names are kept after conversion to numeric
+      names(Ymod_num) <- names(Ymod)
       
       # Perform QPsolve with the new modified Ymod and X
-      QPsolve(Ymod, X)
+      result <- QPsolve(Ymod_num, X)
+      
+      # Convert result to a single row df and give name of id
+      result <- t(as.data.frame(result))
+      rownames(result) <- id
+      
+      return(result)
     }
   }
 }
