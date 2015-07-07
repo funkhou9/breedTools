9154b746f372cf35485ab04fe371a948d6d22e95
Scott Funkhouser <sfunkhouser@gmail.com>
2015-07-07 19:48:38 -0400

    improved efficiency

--- R/has_progeny.R
+++ R/has_progeny.R
@@ -13,32 +13,39 @@
 #' @export
 has_progeny <- function(id, ped, geno = NULL) {
   
-  # Check for proper pedigree format
-  if (!all(c("Sire", "Dam") %in% names(ped))) {
+#   # Check for proper pedigree format
+  if (!all(c("Sire", "Dam") %in% colnames(ped))) {
     stop("Can't recognize pedigree format. Requires 'Sire' and 'Dam' columns")
   }
+#   
+#   # Check if id is a sire
+#   if (id %in% ped[, "Sire"]) {
+#     proj <- ped[ped[, "Sire"] == id, 1]
+#   
+#   # Else check if id is a dam
+#   } else if (id %in% ped[, "Dam"]) {
+#     proj <- ped[ped[, "Dam"] == id, 1]
+#   
+#   # Case when no projeny present
+#   } else {
+#     proj <- NULL
+#     return(NA)
+#   }
+#   
+#   # If genotyping data is provided - return the number
+#   #   of progeny genotyped
+#   if (!is.null(geno)) {
+#     return (sum(proj %in% rownames(geno)))  
+#   
+#   # Otherwise just return the number of progeny
+#   } else {
+#     return (length(proj))
+#   }
+#   
   
-  # Check if id is a sire
-  if (id %in% ped[, "Sire"]) {
-    proj <- ped[ped[, "Sire"] == id, 1]
-  
-  # Else check if id is a dam
-  } else if (id %in% ped[, "Dam"]) {
-    proj <- ped[ped[, "Dam"] == id, 1]
-  
-  # Case when no projeny present
-  } else {
-    proj <- NULL
-    return(NA)
-  }
-  
-  # If genotyping data is provided - return the number
-  #   of progeny genotyped
   if (!is.null(geno)) {
-    return (sum(proj %in% rownames(geno)))  
-  
-  # Otherwise just return the number of progeny
+    return (sum(ped[ped[, "Sire"] == id, 1] %in% rownames(geno)))
   } else {
-    return (length(proj))
+    return (length(ped[ped[, "Sire"] == id, 1]))
   }
 }
