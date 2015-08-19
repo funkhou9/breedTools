68786b07d87efdf7502191a8bc6273495db8970a
Scott Funkhouser <sfunkhouser@gmail.com>
2015-08-19 15:38:05 -0400

    initial commit

---
+++ R/get_gsibs.R
@@ -0,0 +1,33 @@
+#' Obtain relatives of id that share a grand mother or grand father, if any relatives
+#'  
+#' @param id character naming ID of interest
+#' @param ped data.frame consisting of pedigree information
+#'  with columns {ID, Sire, Dam} in that order
+#' @return character vector of sibling IDs
+#' @export
+has_gsib <- function(id, ped) {
+  
+  # Check for proper pedigree format
+  if (!all(c("Sire", "Dam") %in% names(ped)))
+    stop("Can't recognize pedigree format. Requires 'Sire' and 'Dam' columns")
+  
+  # Obtain parents of id
+  if (id %in% ped[, 1]) {
+    sire_id <- ped[ped[, 1] == id, "Sire"]
+    dam_id <- ped[ped[, 1] == id, "Dam"]
+    
+    g_sire_id <- ped[ped[, 1] == sire_id, "Sire"]
+    g_dam_id <- ped[ped[, 1] == sire_id, "Dam"]
+  } else
+      return (NULL)
+  
+  # Get 'grand sibs'
+  g_sire_proj <- ped[ped[, 2] == g_sire_id, 1]
+  g_dam_proj <- ped[ped[, 3] == g_dam_id, 1]
+  
+  sib_sires <- ped[ped[, 2] %in% g_sire_proj, 1]
+  sib_dams <- ped[ped[, 2] %in% g_dam_proj, 1]
+  
+  # Obtain the 'grand sibs' (including id)
+  c(sib_sires, sib_dams)
+}
---
+++ R/get_sibs.R
@@ -0,0 +1,28 @@
+#' Obtain siblings of id, if any
+#' 
+#'  
+#' @param id character naming ID of interest
+#' @param ped data.frame consisting of pedigree information
+#'  with columns {ID, Sire, Dam} in that order
+#' @return character vector of sibling IDs
+#' @export
+get_sibs <- function(id, ped) {
+  
+  # Check for proper pedigree format
+  if (!all(c("Sire", "Dam") %in% names(ped)))
+    stop("Can't recognize pedigree format. Requires 'Sire' and 'Dam' columns")
+  
+  # Obtain parents of id
+  if (id %in% ped[, 1]) {
+    sire_id <- ped[ped[, 1] == id, "Sire"]
+    dam_id <- ped[ped[, 1] == id, "Dam"]
+  } else
+      return (NULL)
+  
+  # Get sib IDs
+  sibs_from_sire <- ped[ped[, 2] == sire_id, 1]
+  sibs_from_dam <- ped[ped[, 3] == dam_id, 1]
+  
+  # Obtain sibs! (including id)
+  c(sibs_from_sire, sibs_from_dam)
+}
