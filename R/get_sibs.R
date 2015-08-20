183967738f8b67fb031a6d6af6fd9a60c22cf1f9
Scott Funkhouser <sfunkhouser@gmail.com>
2015-08-19 21:34:03 -0400

    added arg for retrieving siblings related by grand parents

--- R/get_sibs.R
+++ R/get_sibs.R
@@ -4,9 +4,11 @@
 #' @param id character naming ID of interest
 #' @param ped data.frame consisting of pedigree information
 #'  with columns {ID, Sire, Dam} in that order
+#' @param gsib logical. If true, finds siblings related by parents
+#'  and grand parents
 #' @return character vector of sibling IDs
 #' @export
-get_sibs <- function(id, ped) {
+get_sibs <- function(id, ped, gsib = TRUE) {
   
   # Check for proper pedigree format
   if (!all(c("Sire", "Dam") %in% names(ped)))
@@ -16,6 +18,12 @@ get_sibs <- function(id, ped) {
   if (id %in% ped[, 1]) {
     sire_id <- ped[ped[, 1] == id, "Sire"]
     dam_id <- ped[ped[, 1] == id, "Dam"]
+    
+    if (gsib) {
+      g_sire_id <- ped[ped[, 1] == sire_id, "Sire"]
+      g_dam_id <- ped[ped[, 1] == sire_id, "Dam"]
+    }
+    
   } else
       return (NULL)
   
@@ -23,6 +31,22 @@ get_sibs <- function(id, ped) {
   sibs_from_sire <- ped[ped[, 2] == sire_id, 1]
   sibs_from_dam <- ped[ped[, 3] == dam_id, 1]
   
+  if (gsib) {
+    g_sire_proj <- ped[ped[, 2] == g_sire_id, 1]
+    g_dam_proj <- ped[ped[, 3] == g_dam_id, 1]
+    gsibs_from_sire <- ped[ped[, 2] %in% g_sire_proj, 1]
+    gsibs_from_dam <- ped[ped[, 2] %in% g_dam_proj, 1]    
+  }
+
   # Obtain sibs! (including id)
-  unique(c(sibs_from_sire, sibs_from_dam))
+  if (gsib)
+    siblings <- unique(c(sibs_from_sire,
+                         sibs_from_dam,
+                         gsibs_from_sire,
+                         gsibs_from_dam))
+  else
+    siblings <- unique(c(sibs_from_sire,
+                         sibs_from_dam))
+  
+  siblings
 }
