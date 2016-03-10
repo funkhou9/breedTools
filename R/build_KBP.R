b16f50ee54538033a522fb9943eb5e0477f3b816
Scott Funkhouser <sfunkhouser@gmail.com>
2016-03-10 10:00:05 -0500

    update docs and return breed probabilities

--- R/build_KBP.R
+++ R/build_KBP.R
@@ -2,8 +2,8 @@
 #' 
 #' KBP (Kit-based breed probabilities) utilize a reference panel of purebred animals' genotypes.
 #' The genotypes are phased, and the resulting haplotypes are used to construct haplotype frequences,
-#' genotype frequencies given breed $P(g|b)$, and finally breed probabilities given a genotype
-#' $P(b | g)$. This function is used to construct a reference panel of breed probabilities.
+#' genotype frequencies given breed \eqn{P(g | b)}, and finally breed probabilities given a genotype
+#' \eqn{P(b | g)}. This function is used to construct a reference panel of breed probabilities.
 #' 
 #' @param geno a matrix or data.frame with genotypes of reference animals. SNPs in columns and 
 #' individuals in rows. Genotypes must be coded as dosage of allele 'b' {0, 1, 2}. 
@@ -14,6 +14,8 @@
 #' resides along PATH.
 #' @param groups a list of character vectors with the names of IDs meant to be processed as groups. 
 #' Names of list should be the names of the groups, with each element containing IDs.
+#' @param haplotypes logical indicating whether build_KBP should return haplotype frequencies (TRUE)
+#' or breed probabilities (FALSE).
 #' @import snpTools
 #' @import magrittr
 #' @export
@@ -23,7 +25,8 @@ build_KBP <- function(geno,
                       map,
                       ped = NULL,
                       path = NULL,
-                      groups = NULL) {
+                      groups = NULL,
+                      haplotypes = FALSE) {
   
   if (is.null(groups))
     stop("build_KBP() is designed to be used with a groups arugment, corresponding to a list of reference breeds")
@@ -46,8 +49,9 @@ build_KBP <- function(geno,
                         exclude_chr = "1 2 3 4 5 6 7 9 10 11 12 13 14 15 16 17 18 19 20 21")
   message("Obtaining reference haplotypes from FImpute")
   
-  breed_list <- list()
-  # Obtain haplotypes from each group (each group should correspond to each reference breed)
+  # Obtain haplotypes and haplotype frequencies from each reference breed
+  hap_list <- list()
+  hap_freq_list <- list()
   for (i in 1:length(groups)) {
     
     # Get SNP names
@@ -57,37 +61,86 @@ build_KBP <- function(geno,
                 as.character()
     
     # Read haplotype file as a single string
-    breed_list[[i]] <- read.table(paste0(names(groups)[i], "_fimpute_run/hap_library.txt"),
+    hap_list[[i]] <- read.table(paste0(names(groups)[i], "_fimpute_run/hap_library.txt"),
                                   skip = 1, 
                                   colClasses = "character")
     
     # Convert to a vector with snpTools::string_to_vec()
-    breed_list[[i]] <- t(apply(breed_list[[i]], 1, snpTools::string_to_vec))
+    hap_list[[i]] <- t(apply(hap_list[[i]], 1, snpTools::string_to_vec))
     
     # Subset KIT SNPs
-    colnames(breed_list[[i]]) <- snps
-    breed_list[[i]] <- breed_list[[i]][, kit_snps]
+    colnames(hap_list[[i]]) <- snps
+    hap_list[[i]] <- hap_list[[i]][, kit_snps]
     
     # Remove any haplotypes with missing data (marked by the int 5)
-    breed_list[[i]] <- breed_list[[i]][apply(breed_list[[i]] != "5", 1, all), ]
+    hap_list[[i]] <- hap_list[[i]][apply(hap_list[[i]] != "5", 1, all), ]
     
     # Convert coding to dosage of allele 2
-    breed_list[[i]] <- t(apply(breed_list[[i]], 1, function(x) as.numeric(x) - 1))
-    breed_list[[i]] <- t(apply(breed_list[[i]], 1, as.character))
+    hap_list[[i]] <- t(apply(hap_list[[i]], 1, function(x) as.numeric(x) - 1))
+    hap_list[[i]] <- t(apply(hap_list[[i]], 1, as.character))
     
     # Compute haplotype frequencies and tabulate results
-    breed_list[[i]] <- haplo_comp(breed_list[[i]], haplotypes = TRUE)
-    breed_list[[i]] <- data.frame("haplotypes" = names(breed_list[[i]]),
-                                  "frequency" = breed_list[[i]],
-                                  stringsAsFactors = FALSE)
+    hap_freq_list[[i]] <- haplo_comp(hap_list[[i]], haplotypes = TRUE)
+    hap_freq_list[[i]] <- data.frame("haplotypes" = names(hap_list[[i]]),
+                                     "frequency" = hap_list[[i]],
+                                     stringsAsFactors = FALSE)
   }
   
-  # Merge haplotypes
-  hap_data <- breed_list[[1]]
-  for (i in 2:length(breed_list)) {
-    hap_data <- merge(hap_data, breed_list[[i]], by = "haplotypes", all = TRUE)
+  if (haplotypes) {
+    # Merge haplotype frequencies for optional return of reference haplotype information
+    hap_data <- hap_freq_list[[1]]
+    for (i in 2:length(hap_freq_list)) {
+      hap_data <- merge(hap_data, hap_freq_list[[i]], by = "haplotypes", all = TRUE)
+    }
+    colnames(hap_data) <- c("Haplotype", names(groups))
+    return(hap_data)  
   }
   
-  colnames(hap_data) <- c("Haplotype", names(groups))
-  return(hap_data)
+  
+  # Compute "within breed" genotype probabilities
+  within_breed <- list()
+  for (i in 1:length(hap_list)) {
+    hap_freq_i <- haplo_comp(hap_list[[i]])
+    within_breed[[i]] <- data.frame("G" = names(hap_freq_i),
+                                    "pG" = hap_freq_i,
+                                    stringsAsFactors = FALSE)
+  }
+  
+  # Compute "between breed" genotype probabilities (with counter to count the number of combinations)
+  between_breed <- list()
+  n_between <- 0
+  for (i in 1:(length(hap_list) - 1)) {
+    for (j in (i + 1):length(hap_list)) {
+      hap_cross_freq_ij <- haplo_comp(hap_list[[i]], hap_list[[j]])
+      between_breed <- c(between_breed, 
+                         data.frame("G" = names(hap_cross_freq_ij),
+                                    "pG" = hap_cross_freq_ij,
+                                    stringsAsFactors = FALSE))
+      n_between <- n_between + 1
+    }
+  }
+  
+  # Combine probabilities and join by "G" (genotype)
+  g_mat_list <- c(within_breed, between_breed)
+  g_data <- g_mat_list[[1]]
+  for (i in 2:length(g_mat_list)) {
+    g_data <- merge(g_data, g_mat_list[[i]], by = "G", all = TRUE)
+  }
+  
+  # Change NAs in g_data to 0
+  g_data[is.na(g_data)] <- 0
+  
+  # Adjust g_data so that genotypes (first column) are in rownames
+  # g_data should be a data.frame with (length(groups) + n_between) columns, corresponding to the 
+  # number of "within breed" and "between breed" genotype probabilities
+  rownames(g_data) <- g_data[, 1]
+  g_data <- g_data[, 2 : (length(groups) + n_between)]
+  
+  # Calculate b_mat - breed probabilities
+  b_data <- t(apply(g_data, 1, function(x) x / sum(x)))
+  
+  return(b_data)
 }
+
+
+
