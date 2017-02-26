ec98842dd1fb6cea18a9ff69fc185c805c624932
Scott Funkhouser <sfunkhouser@gmail.com>
2017-02-25 21:31:37 -0500

    enable hampshire testing
    using mbp_ref argument, hampshire animals are assumed to be tested. If this is the case, output is different from when Yorkshire/Landrace animals are being tested.

--- R/screen_purity.R
+++ R/screen_purity.R
@@ -12,7 +12,8 @@
 #' @param kbp_ref data.frame containing genotype probabilities for various multi-locus genotypes
 #' surrounding the KIT region (the output of build_KBP).
 #' @param mbp_ref data.frame containing genotype probabilities for various multi-locus genotypes
-#' surrounding the MC1R region (the output of build_MBP). (OPTIONAL)
+#' surrounding the MC1R region (the output of build_MBP). (OPTIONAL) If supplied, it will
+#' assume Hampshire animals are being evaluated in \code{geno}.
 #' @return data.frame with various GWBC and KBP values for each animal in geno
 #' @export
 screen_purity <- function(geno,
@@ -26,19 +27,60 @@ screen_purity <- function(geno,
   # Solve KBP
   kbp_res <- solve_KBP(geno, kbp_ref)
   
-  # Join GWBC and KBP results keeping only values related to 'white' KBPs.
-  results <- data.frame(gwbc_res[rownames(kbp_res), 1:4],
-                        kbp_res[, c(3, 5, 6)],
-                        kit_analysis_inconclusive = rep("no", nrow(geno)),
-                        stringsAsFactors = FALSE)
-  colnames(results) <- c("GWBC_Duroc",
-                         "GWBC_Hampshire",
-                         "GWBC_Landrace",
-                         "GWBC_Yorkshire",
-                         "KBP_White",
-                         "KBP_Duroc_White",
-                         "KBP_Hampshire_White",
-                         "KIT_inconclusive")
+  # If mbp_ref is NULL, Yorkshire or Landrace animals are being evaluated.
+  if (is.null(mbp_ref)) {
+    # Join GWBC and KBP results keeping only values related to 'white' KBPs.
+    results <- data.frame(gwbc_res[rownames(kbp_res), 1:4],
+                          kbp_res[, c(3, 5, 6)],
+                          rep("no", nrow(geno)),
+                          stringsAsFactors = FALSE)
+    colnames(results) <- c("GWBC_Duroc",
+                           "GWBC_Hampshire",
+                           "GWBC_Landrace",
+                           "GWBC_Yorkshire",
+                           "KBP_White",
+                           "KBP_Duroc_White",
+                           "KBP_Hampshire_White",
+                           "KIT_inconclusive")  
+  } else {
+    # Here, Hampshire are presumably being evaluated. First estimate MC1R-based
+    # probabilities
+    mbp_res <- solve_MBP(geno, mbp_ref)
+    
+    # Join GWBC and KBP/MBP results. For KBP/MBP, provide purebred Hampshire
+    # probability and the probability that is the highest (or second highest,
+    # if purebred Hampshire is the highest)
+    kbp_max_idx <- apply(kbp_res, 1, function(x) which(x == max(x)))
+    kbp_max <- colnames(kbp_res)[kbp_max_idx]
+    kbp_max_prob <- kbp_res[cbind(1:nrow(kbp_res), kbp_max_idx)]
+    
+    mbp_max_idx <- apply(mbp_res, 1, function(x) which(x == max(x)))
+    mbp_max <- colnames(mbp_res)[mbp_max_idx]
+    mbp_max_prob <- mbp_res[cbind(1:nrow(mbp_res), mbp_max_idx)]
+    
+    results <- data.frame(gwbc_res[rownames(kbp_res), 1:4],
+                          kbp_res[, "Hampshire"],
+                          kbp_max,
+                          kbp_max_prob,
+                          rep("no", nrow(geno)),
+                          mbp_res[, "Hampshire"],
+                          mbp_max,
+                          mbp_max_prob,
+                          rep("no", nrow(geno)),
+                          stringsAsFactors = FALSE)
+    colnames(results) <- c("GWBC_Duroc",
+                           "GWBC_Hampshire",
+                           "GWBC_Landrace",
+                           "GWBC_Yorkshire",
+                           "KBP_Hampshire",
+                           "Max_KBP",
+                           "KBP_Max",
+                           "KIT_inconclusive",
+                           "MBP_Hampshire",
+                           "Max_MBP",
+                           "MBP_Max",
+                           "MC1R_inconclusive")  
+  }
   
   # Find which animals, if any, have an incomplete set of SNPs in the KIT region
   kit_geno <- geno[, kit_snps]
@@ -46,29 +88,15 @@ screen_purity <- function(geno,
     rownames(kit_geno)[rowSums(is.na(kit_geno)) > 0]
   
   # Fill in which animals were "KIT inconclusive"
-  results[inconclusives, 8] <- "yes"
+  results[inconclusives, "KIT_inconclusive"] <- "yes"
   
-  # If provided a reference panel for MBP, solve MBP and attach MBP results.
-  # Repeat the process of identifying 'MC1R_inconclusive' results.
   if (!is.null(mbp_ref)) {
-    mbp_res <- solve_MBP(geno, mbp_ref)
-    results_mbp <- cbind(results,
-                         mbp_res[rownames(kbp_res), c(2, 5, 8, 9)],
-                         "MC1R_inconclusive" = rep("no", nrow(geno)),
-                         stringsAsFactors = FALSE)
-    colnames(results_mbp) <- c(colnames(results),
-                               "MBP_Hampshire",
-                               "MBP_Hampshire_Duroc",
-                               "MBP_Hampshire_Landrace",
-                               "MBP_Hampshire_Yorkshire",
-                               "MC1R_inconclusive")
-    
     mc1r_geno <- geno[, mc1r_snps]
     inconclusives <-
       rownames(mc1r_geno)[rowSums(is.na(mc1r_geno)) > 0]
 
-    results_mbp[inconclusives, "MC1R_inconclusive"] <- "yes"
-    return(results_mbp)
+    results[inconclusives, "MC1R_inconclusive"] <- "yes"
+    return(results)
   }
   
   return (results)
